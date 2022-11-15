8f54ef3e07f31f085fb951a12917ee8194cea7d2
liff <liff10@asiainfo.com>
2022-11-15 15:00:45 +0800

    Initial commit

---
+++ src/main/java/sc/suancaiyu/demo/GamePlatFormApplication.java
@@ -0,0 +1,13 @@
+package sc.suancaiyu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlatFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamePlatFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sc/suancaiyu/demo/GamePlatFormApplicationTests.java
@@ -0,0 +1,13 @@
+package sc.suancaiyu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePlatFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
